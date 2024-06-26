a0c5b88f3c650a61e0f2f8ca77448e0a0190849b
Hinako Abe <makeitshine0028@yahoo.co.jp>
2024-06-26 12:57:01 +0900

    Controllerと接続

--- app/Http/Controllers/WorkController.php
+++ app/Http/Controllers/WorkController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Work;
 
 class WorkController extends Controller
 {
     //
+    function index()
+    {
+        $works = Work::all();
+        dd($works);
+        return view('works.index');
+    }
 }
