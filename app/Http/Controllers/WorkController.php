9af63150e932d7bd69ad13394cf27b1aedccd3aa
Hinako Abe <makeitshine0028@yahoo.co.jp>
2024-06-27 08:31:59 +0900

    新規投稿と新規作成と保存機能のバックエンド記述

--- app/Http/Controllers/WorkController.php
+++ app/Http/Controllers/WorkController.php
@@ -4,14 +4,36 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Work;
+use Illuminate\Support\Facades\Auth;
 
 class WorkController extends Controller
 {
-    //
+    //詳細ページ
     function index()
     {
         $works = Work::all();
         // dd($works);
         return view('works.index', compact('works'));
+    }
+
+    //新規作成ページ
+    function create()
+    {
+        return view('works.create');
+    }
+
+    //保存機能
+    function store(Request $request)
+    {
+        // dd($request);
+        $work = new Work;
+
+        $work -> title = $request -> title;
+        $work -> body = $request -> body;
+        $work -> user_id = Auth::id();
+
+        $work -> save();
+
+        return redirect()->route('works.index');
     }
 }
