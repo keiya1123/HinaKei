794f03c1daf88c697b34a907f29e24a6a99a17ab
Hinako Abe <makeitshine0028@yahoo.co.jp>
2024-06-29 16:53:49 +0900

    コメント機能

---
+++ app/Http/Controllers/CommentController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Work;
+use App\Models\Comment;
+use Illuminate\Support\Facades\Auth;
+
+class CommentController extends Controller
+{
+    //コメント機能
+    public function create ($work_id)
+    {
+        $work = Work::find($work_id);
+        return view('comments.create' , ['work'=>$work]);
+    }
+
+    public function store(Request $request)
+    {
+        $work = Work::find($request->work_id);
+
+        $comment = new Comment;
+        $comment -> contents = $request -> contents;
+        $comment -> user_id = Auth::id();
+        $comment -> work_id = $request -> work_id;
+
+        $comment -> save();
+
+        return redirect()->route('works.show', $work->id);
+    }
+}
---
+++ app/Models/Comment.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+
+    //コメント機能
+    public function work()
+    {
+        return $this->belongsTo('App\Models\Work');
+    }
+
+    //ユーザーの連携
+    public function user()
+    {
+        return $this->belongsTo('App\Models\User');
+    }
+}
