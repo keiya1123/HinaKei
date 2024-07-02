8ae273e8ecc257f9baed2f0c6b714903e9c5ca4a
Hinako Abe <makeitshine0028@yahoo.co.jp>
2024-07-02 23:46:55 +0900

    詳細ページの見た目変更

--- resources/views/works/show.blade.php
+++ resources/views/works/show.blade.php
@@ -25,18 +25,18 @@
     @endif
 </div>
     <div class="">
-        <div class="card-header tw-text-left tw-bg-black-100 tw-mb-50 " style="font-size: 20px">投稿者：{{ $work->user->name }}</div>
+        <div class=" tw-text-left tw-bg-black-100 tw-mb-50 " style="font-size: 20px">投稿者：{{ $work->user->name }}</div>
     </div> 
   <div class="container">
       <div class="row justify-content-center">
           <div class="col-md-8">
-              <div class="card mt-3">
-                  <div class="card-header">
-                      <h5>目標：{{ $work->title }}</h5>
+              <div class="mt-3">
+                  <div class=""style="font-size: 40px">
+                      <h5 class="tw-text-center">目標：{{ $work->title }}</h5>
                   </div>
-                  <div class="card-body">
-                  <p class="card-text">詳細：{{ $work->contents }}</p>
-                  <p>投稿日時：{{ $work->created_at }}</p>
+                  <div class="">
+                  <p class="tw-text-center tw-text-2xl">詳細：{{ $work->contents }}</p>
+                  <p class="tw-text-right">投稿日時：{{ substr($work->created_at, 0, 10) }}</p>
 
                   @can('poster', $work)
                   <a href="{{ route('works.edit' , $work->id) }}" class="btn btn-primary">編集する</a>
@@ -59,18 +59,20 @@
       </div>
       <div class="row justify-content-center">
         <div class="col-md-8 mt-5">
-          コメント一覧
+          <div class="tw-text-lg" style="font-size: 20px">コメント一覧</div>
           @foreach($work->comments as $comment)
             <div class="card mt-3">
                 <h5 class="tw-bg-red-100 card-header">コメント者：{{ $comment->user->name }}</h5>
                 <div class="tw-bg-blue-100 card-body">
-                    <h5 class="card-title">投稿日時：{{ $comment->created_at }}</h5>
+                    <h5 class="card-title">投稿日時：{{ substr($comment->created_at, 0, 10) }}</h5>
                     <p class="card-text">詳細：{{ $comment->contents }}</p>
                 </div>
                 <form action='{{ route('comments.destroy' , $comment) }}' method='post'>
                   @csrf
                   @method('delete')
-                    <input type='submit' value='削除' class="btn btn-danger" onclick='return confirm("本当に削除しますか？");'>
+                <div class="tw-bg-blue-100">
+                    <input type='submit' value='削除' class="btn btn-danger " onclick='return confirm("本当に削除しますか？");'>
+                  </div>
                 </form>
             </div>
             @endforeach
