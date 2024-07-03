5423acad175e066c5a2dfa34588dbda40963ec43
Hinako Abe <makeitshine0028@yahoo.co.jp>
2024-07-03 14:00:57 +0900

    commentページに一覧ページへのボタン作成

--- resources/views/comments/create.blade.php
+++ resources/views/comments/create.blade.php
@@ -6,10 +6,10 @@
       <div class="col-md-8">
         <h2>以下の目標にコメントします</h2>
           <div class="card mt-3">
-              <div class="card-header">
+              <div class="card-header tw-bg-red-100">
                   <h5>投稿者：{{ $work->id }}</h5>
               </div>
-              <div class="card-body">
+              <div class="card-body tw-bg-blue-100">
             <h5>目標：{{ $work->title }}</h5>
               <p class="card-text">詳細：{{ $work->contents }}</p>
               <p>投稿日時：{{ substr($work->created_at, 0, 10) }}</p>
@@ -18,6 +18,12 @@
           </div>
       </div>
   </div>
+  <div class="col-md-8 tw-text-right">
+    <a href="{{ route('works.index') }}" class="btn btn-danger m-1">一覧に戻る</a>
+</div>
+{{-- <div class="col-md-8 tw-text-right">
+    <a href="{{ route('works.show') }}" class="btn btn-danger m-1">前に戻る</a>
+</div> --}}
   <div class="row justify-content-center mt-5">
     <div class="col-md-8">
         <form action="{{ route('comments.store') }}" method="post">
