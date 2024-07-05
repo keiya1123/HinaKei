3d5402b2ae2c302f738bcc4182a629e22e8fd6eb
keiya1123 <keiya.sato@i.softbank.jp>
2024-07-05 09:51:18 +0800

    月の変更

--- resources/views/works/edit.blade.php
+++ resources/views/works/edit.blade.php
@@ -35,18 +35,18 @@
                 <div class="form-group">
                     <label style="font-size: 20px; margin-bottom: 5px;">何月の目標</label><br>
                     <select name="pulldown" id="pulldown" placeholder="〜月" style="margin-bottom: 20px; width:90px; font-size:18px;">
-                        <option value="1月" >1月</option>
-                        <option value="2月" >2月</option>
-                        <option value="3月" >3月</option>
-                        <option value="4月" >4月</option>
-                        <option value="5月" >5月</option>
-                        <option value="6月" >6月</option>
-                        <option value="7月" >7月</option>
-                        <option value="8月" >8月</option>
-                        <option value="9月" >9月</option>
-                        <option value="10月">10月</option>
-                        <option value="11月">11月</option>
-                        <option value="12月">12月</option>
+                        <option value="1" >1月</option>
+                        <option value="2" >2月</option>
+                        <option value="3" >3月</option>
+                        <option value="4" >4月</option>
+                        <option value="5" >5月</option>
+                        <option value="6" >6月</option>
+                        <option value="7" >7月</option>
+                        <option value="8" >8月</option>
+                        <option value="9" >9月</option>
+                        <option value="10">10月</option>
+                        <option value="11">11月</option>
+                        <option value="12">12月</option>
                     </select>
                 </div>
                 <div class="form-group">
